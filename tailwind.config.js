fbc0dddda49aab3cc7b96700da6963928fdf7a15
Oscar Gutierrez <oscarfgutierrezo@gmail.com>
2022-03-31 11:15:25 -0500

    Add: feature-01 to feature-05 sections

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        "background": "#F2F2F2",
+        "primary": "#2F1893",
+        "dark-violet": "#1E0E62",
+        "dark-violet-02": "#151439",
+        "light-violet": "#482BE7",
+        "pink": "#E93A7D",
+        "green": "#25DAC5",
+        "grey": "#EBEAED",
+        "red": "#EA3223",
+      },
+      fontFamily: {
+        "sans": "DM Sans",
+      },
+      backgroundImage: {
+        feat04: 'url("../images/feature-04.png")',
+      },
+    },
+  },
+  plugins: [],
+}
